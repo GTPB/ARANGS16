f0bf8e651b1c98fd35b543ae34a824916244e343
Jordi Salmona <jordi.salmona@gmail.com>
2016-05-10 17:26:55 +0100

    message

--- conf/vagrant/manifests/default.pp
+++ conf/vagrant/manifests/default.pp
@@ -82,6 +82,14 @@ exec {
 		command   => 'ln -s /usr/local/src/samtools-1.2/samtools /usr/local/bin/samtools',
 		creates   => '/usr/local/bin/samtools',       
 		require   => Exec[ 'make_samtools' ];
+
+        # clone github repository
+        'cl_gihub':
+                command   => 'git clone https://github.com/rvosa/arangs2016.git',
+                cwd       => '/home/vagrant/',
+                creates   => '/home/vagrant/arangs2016',
+                require   => Package[ 'git' ];
+   
 }
 
 
